29917e3871cebf80a0a36a5009c8adfc467d828c
FelineAlloy <luxin.gabriel@gmail.com>
2023-10-15 07:12:47 +0300

    removed introduction empty lesson

--- static/locale/ro.js
+++ static/locale/ro.js
@@ -1,97 +1,93 @@
-if (typeof locales == 'undefined') locales = {};
+if (typeof locales == "undefined") locales = {};
 locales["ro"] = {
-  "Lessons": {
-    "message": "Lecții"
-  },
-  "Exercices": {
-    "message": "Exerciții"
-  },
+	Lessons: {
+		message: "Lecții",
+	},
+	Exercices: {
+		message: "Exerciții",
+	},
 
-  // Lessons
-  //Introduction
-  "introduction": {
-    "message": "Noțiuni introductive",
-    "tag": "lesson"
-  },
-  "natureOfLight": {
-    "message": "Natura luminii",
-    "tag": "lesson"
-  },
-  "fermatPrinciple": {
-    "message": "Principiul lui Fermat",
-    "tag": "lesson"
-  },
-  "refractionLaws": {
-    "message": "Legile refracției",
-    "tag": "lesson"
-  },
-  "reflexionLaws": {
-    "message": "Legile reflexiei",
-    "tag": "lesson"
-  },
-  "imgThroughPlaneDiopter": {
-    "message": "Aplicație: Imaginea unui obiect prin dioptrul plan",
-    "tag": "lesson"
-  },
-  "opticalPrism": {
-    "message": "Aplicație: Prisma optică",
-    "tag": "lesson"
-  },
+	// Lessons
+	//Introduction
+	natureOfLight: {
+		message: "Natura luminii",
+		tag: "lesson",
+	},
+	fermatPrinciple: {
+		message: "Principiul lui Fermat",
+		tag: "lesson",
+	},
+	refractionLaws: {
+		message: "Legile refracției",
+		tag: "lesson",
+	},
+	reflexionLaws: {
+		message: "Legile reflexiei",
+		tag: "lesson",
+	},
+	imgThroughPlaneDiopter: {
+		message: "Aplicație: Imaginea unui obiect prin dioptrul plan",
+		tag: "lesson",
+	},
+	opticalPrism: {
+		message: "Aplicație: Prisma optică",
+		tag: "lesson",
+	},
 
-  //The Spherical Diopter
-  "sphericalDiopter": {
-    "message": "Dioptrul sferic",
-    "tag": "lesson"
-  },
-  "objectFocus": {
-    "message": "Focare: Focar obiect",
-    "tag": "lesson"
-  },
-  "imageFocus": {
-    "message": "Focare: Focar imagine",
-    "tag": "lesson"
-  },
-  "transverseGrowth": {
-    "message": "Marirea liniară transversală",
-    "tag": "lesson"
-  },
-  "planeDiopter": {
-    "message": "Dioptrul plan",
-    "tag": "lesson"
-  },
-  "sphericalMirror": {
-    "message": "Oglinzi sferice",
-    "tag": "lesson"
-  },
+	//The Spherical Diopter
+	sphericalDiopter: {
+		message: "Dioptrul sferic",
+		tag: "lesson",
+	},
+	objectFocus: {
+		message: "Focare: Focar obiect",
+		tag: "lesson",
+	},
+	imageFocus: {
+		message: "Focare: Focar imagine",
+		tag: "lesson",
+	},
+	transverseGrowth: {
+		message: "Marirea liniară transversală",
+		tag: "lesson",
+	},
+	planeDiopter: {
+		message: "Dioptrul plan",
+		tag: "lesson",
+	},
+	sphericalMirror: {
+		message: "Oglinzi sferice",
+		tag: "lesson",
+	},
 
-  // Lenses
-  "lenses": {
-    "message": "Lentile subțiri",
-    "tag": "lesson"
-  },
+	// Lenses
+	lenses: {
+		message: "Lentile subțiri",
+		tag: "lesson",
+	},
 
-  // Optical Instruments
-  "instruments": {
-    "message": "Instrumente optice",
-    "tag": "lesson"
-  },
-  "magnifyingGlass": {
-    "message": "Lupa",
-    "tag": "lesson"
-  },
-  "microscope": {
-    "message": "Microscopul",
-    "tag": "lesson"
-  },
-  "telescope": {
-    "message": "Telescopul",
-    "tag": "lesson"
-  },
+	// Optical Instruments
+	instruments: {
+		message: "Instrumente optice",
+		tag: "lesson",
+	},
+	magnifyingGlass: {
+		message: "Lupa",
+		tag: "lesson",
+	},
+	microscope: {
+		message: "Microscopul",
+		tag: "lesson",
+	},
+	telescope: {
+		message: "Telescopul",
+		tag: "lesson",
+	},
 
-  // Errors
-  "errorExist": {
-    "message": "This lesson does not exist!"
-  },
-}
+	// Errors
+	errorExist: {
+		message: "This lesson does not exist!",
+	},
+};
 
-module.exports = {locales: locales};
+module.exports = { locales: locales };
